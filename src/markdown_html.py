103f2ad31e630811765a14a9591847b02a709561
melvintu <melvin@meldano.com>
2026-01-07 01:00:13 +1100

    Cleaned up some of the markdown_html code, extra helper functions to clean up the text

--- src/markdown_html.py
+++ src/markdown_html.py
@@ -1,6 +1,6 @@
 from blocks_markdown import markdown_to_blocks, block_to_block_type, BlockType
 from textnode import text_node_to_html_node, TextNode, TextType
-from htmlnode import HTMLNode, LeafNode, ParentNode
+from htmlnode import ParentNode
 from inline_markdown import text_to_textnodes
 
 def markdown_to_html_node(markdown):
@@ -14,7 +14,8 @@ def markdown_to_html_node(markdown):
 def block_to_parent(text):
     block_type = block_to_block_type(text)
     if block_type == BlockType.QUOTE:
-        children_nodes = text_to_children(text)
+        cleaned_text = clean_text_blockquotes(text)
+        children_nodes = text_to_children(cleaned_text)
         return ParentNode("blockquote", children_nodes)
     
     if block_type == BlockType.UNORDERED_LISTS:
@@ -26,9 +27,10 @@ def block_to_parent(text):
         return ParentNode("ol", tagged_list)
     
     if block_type == BlockType.CODE:
-        children_nodes = text_to_children(text)
-        parent_node = ParentNode("pre", children_nodes)
-        return ParentNode("code", parent_node)
+        inner = strip_code_fence(text)
+        code_text_node = TextNode(inner, TextType.CODE)
+        code_html_node = text_node_to_html_node(code_text_node)
+        return ParentNode("pre", [code_html_node])
 
     if block_type == BlockType.HEADING:
         header_tag_count = heading_tag_count(text)
@@ -63,23 +65,34 @@ def tagging_lines_in_list(text):
         split_lines.pop(0)
     tagged_list = []
     for line in split_lines:
-        child_node = text_to_children(line)
+        if line.startswith("-"):
+            new_line = line[2:]
+        else:
+            new_line = line[3:]
+        child_node = text_to_children(new_line)
         tagged_list.append(ParentNode("li", child_node))
     return tagged_list
 
-# Takes block -> Turns block and splits it into TextNodes
-# Takes each Textnode and turns it into a LeafNode
-# The LeafNodes are then attached to the ParentNode (Which is the block)
-# The block is then attached to the main GrandparentNode(Another parent node)
-# Structure LeafNode (The individual lines of text)
-# -> ParentNode (The block nodes)
-# -> GrandparentNode (The Body)
+def clean_text_blockquotes(text):
+    lines = [line for line in text.splitlines() if line.strip() !=""]
+    pieces = []
+    for line in lines:
+        if line.startswith("> "):
+            pieces.append(line[2:])
+        elif line.startswith(">"):
+            pieces.append(line[1:].lstrip())
+        else:
+            pieces.append(line)
+    return " ".join(pieces)
 
-#Unordered lists, need to be ParentNode(Block for ul) -> Each line is li -> LeafNode
-# Unordered list text needs to be first split into a list
-# So it looks like ["- number 1", "- number 2", "- number **3**"]
-# Then each item in the list gets put through the nodes, then put through li tag, then put through ul tag
-
-#notes - Child node = leaf node, where LeafNode(the tag (span), "the content")
-# then parent node is ParentNode(tag(div), [child_node])
-# then whath appens is parent_node.to_html() becomes <div><span>child</span></div>
+def strip_code_fence(block):
+    lines = block.split("\n")
+    while lines and lines[0].strip() == "":
+        lines.pop(0)
+    while lines and lines[-1].strip() == "":
+        lines.pop()
+    if lines and lines[0].startswith("```"):
+        lines.pop(0)
+    if lines and lines[-1].startswith("```"):
+        lines.pop()
+    return "\n".join(lines) + "\n"
--- src/test_markdown.py
+++ src/test_markdown.py
@@ -1,204 +1,85 @@
 import unittest
+from markdown_html import (
+    markdown_to_html_node,
+    markdown_to_blocks,
+    block_to_block_type,
+)
+from blocks_markdown import BlockType
 
-from textnode import TextNode, TextType
-from inline_markdown import extract_markdown_images, extract_markdown_links, split_nodes_images, split_nodes_link, text_to_textnodes
-from markdown_html import markdown_to_html_node
-
-"""
-class TestMarkdownExtraction(unittest.TestCase):
-    def test_extract_markdown_images(self):
-        matches = extract_markdown_images(
-            "This is text with a ![rick roll](https://i.imgur.com/aKaOqIh.gif) and ![obi wan](https://i.imgur.com/fJRm4Vk.jpeg)"
-        )
-        self.assertListEqual([("rick roll", "https://i.imgur.com/aKaOqIh.gif"), ("obi wan", "https://i.imgur.com/fJRm4Vk.jpeg")], matches)
-
-    def test_extract_markdown_links(self):
-        matches = extract_markdown_links(
-            "This is text with a link [to boot dev](https://www.boot.dev) and [to youtube](https://www.youtube.com/@bootdotdev)"
-        )
-        self.assertListEqual([("to boot dev", "https://www.boot.dev"), ("to youtube", "https://www.youtube.com/@bootdotdev")], matches)
-
-    def test_extract_images_no_result(self):
-        text = "This text has no images."
-        self.assertEqual(extract_markdown_images(text), [])
-
-    def test_extract_links_no_result(self):
-        text = "This text has no links."
-        self.assertEqual(extract_markdown_links(text), [])
-
-    def test_extract_links_ignores_images(self):
-        text = "This is a link [link](url.com) and this is an image ![image](img.png)"
-        matches = extract_markdown_links(text)
-        self.assertListEqual([("link", "url.com")], matches)
-
-    def test_extract_images_ignores_links(self):
-        text = "This is a link [link](url.com) and this is an image ![image](img.png)"
-        matches = extract_markdown_images(text)
-        self.assertListEqual([("image", "img.png")], matches)
-
-    def test_empty_alt_text_and_url(self):
-        text_img = "Here is an image with no alt: ![](https://img.com/a.png)"
-        text_link = "Here is a link with no url: [click me]()"
-        
-        self.assertListEqual([("", "https://img.com/a.png")], extract_markdown_images(text_img))
-        self.assertListEqual([("click me", "")], extract_markdown_links(text_link))
-
-##########################################################################################
-
-    def test_split_images(self):
-        node = TextNode(
-            "This is text with an ![image](https://i.imgur.com/zjjcJKZ.png) and another ![second image](https://i.imgur.com/3elNhQu.png)",
-            TextType.TEXT,
-        )
-        new_nodes = split_nodes_images([node])
-
-        self.assertListEqual(
-            [
-                TextNode("This is text with an ", TextType.TEXT),
-                TextNode("image", TextType.IMAGE, "https://i.imgur.com/zjjcJKZ.png"),
-                TextNode(" and another ", TextType.TEXT),
-                TextNode(
-                    "second image", TextType.IMAGE, "https://i.imgur.com/3elNhQu.png"
-                ),
-            ],
-            new_nodes,
-        )
-
-
-    def test_split_image_text_with_image_first(self):
-        node = TextNode(
-            "![rick roll](https://i.imgur.com/aKaOqIh.gif) and ![obi wan](https://i.imgur.com/fJRm4Vk.jpeg)",
-            TextType.TEXT
-        )
-        new_nodes = split_nodes_images([node])
-        self.assertListEqual(
-            [
-                TextNode("rick roll", TextType.IMAGE, "https://i.imgur.com/aKaOqIh.gif"),
-                TextNode(" and ", TextType.TEXT),
-                TextNode(
-                    "obi wan", TextType.IMAGE, "https://i.imgur.com/fJRm4Vk.jpeg"
-                ),
-            ],
-            new_nodes,
-        )
-    
-    def test_no_images_in_text(self):
-        node = TextNode("This is a text with no images", TextType.TEXT)
-        new_nodes = split_nodes_link([node])
-        self.assertListEqual([TextNode("This is a text with no images", TextType.TEXT)], new_nodes)
-
-    def test_images_ignores_links(self):
-        node = TextNode("This is a link [link](url.com) and this is an image ![image](img.png)", TextType.TEXT)
-        new_nodes = split_nodes_images([node])
-        self.assertListEqual(
-            [
-                TextNode("This is a link [link](url.com) and this is an image ", TextType.TEXT),
-                TextNode("image", TextType.IMAGE, "img.png")
-            ],
-            new_nodes,
-        )
-
-##########################################################################################
-
-    def test_split_link_text(self):
-        node = TextNode(
-            "This is text with a link [to boot dev](https://www.boot.dev) and [to youtube](https://www.youtube.com/@bootdotdev)",
-            TextType.TEXT,
-        )
-        new_nodes = split_nodes_link([node])
-
-        self.assertEqual(
-            [
-                TextNode("This is text with a link ", TextType.TEXT),
-                TextNode("to boot dev", TextType.LINK, "https://www.boot.dev"),
-                TextNode(" and ", TextType.TEXT),
-                TextNode("to youtube", TextType.LINK, "https://www.youtube.com/@bootdotdev"),
-            ], 
-            new_nodes,
-        )
-
-        def test_no_images_in_text(self):
-            node = TextNode("This is a text with no links", TextType.TEXT)
-            new_nodes = split_nodes_link([node])
-            self.assertListEqual([TextNode("This is a text with no links", TextType.TEXT)], new_nodes)
-
-    def test_links_ignores_images(self):
-        node = TextNode("This is a link [link](url.com) and this is an image ![image](img.png)", TextType.TEXT)
-        new_nodes = split_nodes_link([node])
-        self.assertListEqual(
-            [
-                TextNode("This is a link ", TextType.TEXT),
-                TextNode("link", TextType.LINK, "url.com"),
-                TextNode(" and this is an image ![image](img.png)", TextType.TEXT),
-            ],
-            new_nodes,
-        )
-        
-
-##########################################################################################
-
-    def test_complete_split(self):
-        text = "This is **text** with an _italic_ word and a `code block` and an ![obi wan image](https://i.imgur.com/fJRm4Vk.jpeg) and a [link](https://boot.dev)"
-        new_node = text_to_textnodes(text)
-        self.assertListEqual(
-            [
-                TextNode("This is ", TextType.TEXT),
-                TextNode("text", TextType.BOLD),
-                TextNode(" with an ", TextType.TEXT),
-                TextNode("italic", TextType.ITALIC),
-                TextNode(" word and a ", TextType.TEXT),
-                TextNode("code block", TextType.CODE),
-                TextNode(" and an ", TextType.TEXT),
-                TextNode("obi wan image", TextType.IMAGE, "https://i.imgur.com/fJRm4Vk.jpeg"),
-                TextNode(" and a ", TextType.TEXT),
-                TextNode("link", TextType.LINK, "https://boot.dev"),
-            ],
-            new_node
-        )
-        
-    def test_complete_split_no_bold(self):
-        text = "This is text with an _italic_ word and a `code block` and an ![obi wan image](https://i.imgur.com/fJRm4Vk.jpeg) and a [link](https://boot.dev)"
-        new_node = text_to_textnodes(text)
-        self.assertListEqual(
-            [
-                TextNode("This is text with an ", TextType.TEXT),
-                TextNode("italic", TextType.ITALIC),
-                TextNode(" word and a ", TextType.TEXT),
-                TextNode("code block", TextType.CODE),
-                TextNode(" and an ", TextType.TEXT),
-                TextNode("obi wan image", TextType.IMAGE, "https://i.imgur.com/fJRm4Vk.jpeg"),
-                TextNode(" and a ", TextType.TEXT),
-                TextNode("link", TextType.LINK, "https://boot.dev"),
-            ],
-            new_node
-        )
-    def test_complete_split_no_image(self):
-        text = "This is text with an _italic_ word and a `code block` and a [link](https://boot.dev)"
-        new_node = text_to_textnodes(text)
-        self.assertListEqual(
-            [
-                TextNode("This is text with an ", TextType.TEXT),
-                TextNode("italic", TextType.ITALIC),
-                TextNode(" word and a ", TextType.TEXT),
-                TextNode("code block", TextType.CODE),
-                TextNode(" and a ", TextType.TEXT),
-                TextNode("link", TextType.LINK, "https://boot.dev"),
-            ],
-            new_node
-        )
-    def test_with_nothing(self):
-        text = "This is text"
-        new_node = text_to_textnodes(text)
-        self.assertListEqual(
-            [
-                TextNode("This is text", TextType.TEXT),
-            ],
-            new_node
-        )
-
-"""
 
 class TestMarkdownToHTML(unittest.TestCase):
+    def test_markdown_to_blocks(self):
+        md = """
+This is **bolded** paragraph
+
+This is another paragraph with _italic_ text and `code` here
+This is the same paragraph on a new line
+
+- This is a list
+- with items
+"""
+        blocks = markdown_to_blocks(md)
+        self.assertEqual(
+            blocks,
+            [
+                "This is **bolded** paragraph",
+                "This is another paragraph with _italic_ text and `code` here\nThis is the same paragraph on a new line",
+                "- This is a list\n- with items",
+            ],
+        )
+
+    def test_markdown_to_blocks_newlines(self):
+        md = """
+This is **bolded** paragraph
+
+
+
+
+This is another paragraph with _italic_ text and `code` here
+This is the same paragraph on a new line
+
+- This is a list
+- with items
+"""
+        blocks = markdown_to_blocks(md)
+        self.assertEqual(
+            blocks,
+            [
+                "This is **bolded** paragraph",
+                "This is another paragraph with _italic_ text and `code` here\nThis is the same paragraph on a new line",
+                "- This is a list\n- with items",
+            ],
+        )
+
+    def test_block_to_block_types(self):
+        block = "# heading"
+        self.assertEqual(block_to_block_type(block), BlockType.HEADING)
+        block = "```\ncode\n```"
+        self.assertEqual(block_to_block_type(block), BlockType.CODE)
+        block = "> quote\n> more quote"
+        self.assertEqual(block_to_block_type(block), BlockType.QUOTE)
+        block = "- list\n- items"
+        self.assertEqual(block_to_block_type(block), BlockType.UNORDERED_LISTS)
+        block = "1. list\n2. items"
+        self.assertEqual(block_to_block_type(block), BlockType.ORDERED_LISTS)
+        block = "paragraph"
+        self.assertEqual(block_to_block_type(block), BlockType.PARAGRAPH)
+
+    def test_paragraph(self):
+        md = """
+This is **bolded** paragraph
+text in a p
+tag here
+
+"""
+
+        node = markdown_to_html_node(md)
+        html = node.to_html()
+        self.assertEqual(
+            html,
+            "<div><p>This is <b>bolded</b> paragraph text in a p tag here</p></div>",
+        )
+
     def test_paragraphs(self):
         md = """
 This is **bolded** paragraph
@@ -212,25 +93,62 @@ This is another paragraph with _italic_ text and `code` here
         node = markdown_to_html_node(md)
         html = node.to_html()
         self.assertEqual(
-        html,
-        "<div><p>This is <b>bolded</b> paragraph text in a p tag here</p><p>This is another paragraph with <i>italic</i> text and <code>code</code> here</p></div>",
-    )
+            html,
+            "<div><p>This is <b>bolded</b> paragraph text in a p tag here</p><p>This is another paragraph with <i>italic</i> text and <code>code</code> here</p></div>",
+        )
 
-def test_codeblock(self):
-    md = """
-```
-This is text that _should_ remain
-the **same** even with inline stuff
-```
+    def test_lists(self):
+        md = """
+- This is a list
+- with items
+- and _more_ items
+
+1. This is an `ordered` list
+2. with items
+3. and more items
+
 """
 
-    node = markdown_to_html_node(md)
-    html = node.to_html()
-    self.assertEqual(
-        html,
-        "<div><pre><code>This is text that _should_ remain\nthe **same** even with inline stuff\n</code></pre></div>",
-    )
-    def test_codeblock(self):
+        node = markdown_to_html_node(md)
+        html = node.to_html()
+        self.assertEqual(
+            html,
+            "<div><ul><li>This is a list</li><li>with items</li><li>and <i>more</i> items</li></ul><ol><li>This is an <code>ordered</code> list</li><li>with items</li><li>and more items</li></ol></div>",
+        )
+
+    def test_headings(self):
+        md = """
+# this is an h1
+
+this is paragraph text
+
+## this is an h2
+"""
+
+        node = markdown_to_html_node(md)
+        html = node.to_html()
+        self.assertEqual(
+            html,
+            "<div><h1>this is an h1</h1><p>this is paragraph text</p><h2>this is an h2</h2></div>",
+        )
+
+    def test_blockquote(self):
+        md = """
+> This is a
+> blockquote block
+
+this is paragraph text
+
+"""
+
+        node = markdown_to_html_node(md)
+        html = node.to_html()
+        self.assertEqual(
+            html,
+            "<div><blockquote>This is a blockquote block</blockquote><p>this is paragraph text</p></div>",
+        )
+
+    def test_code(self):
         md = """
 ```
 This is text that _should_ remain
@@ -239,13 +157,11 @@ the **same** even with inline stuff
 """
 
         node = markdown_to_html_node(md)
-        #html = node.to_html()
-        """"
+        html = node.to_html()
         self.assertEqual(
             html,
             "<div><pre><code>This is text that _should_ remain\nthe **same** even with inline stuff\n</code></pre></div>",
         )
-        """
 
 
 if __name__ == "__main__":
